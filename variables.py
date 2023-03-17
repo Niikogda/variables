66b2d0017ca638f42974fef53709a4c35274ca70
Laptop <Laptop@DESKTOP-BVI07RL>
2023-03-17 23:20:41 +0200

    task3_done

--- variables.py
+++ variables.py
@@ -1,1 +1,10 @@
 
+
+num1 = int(input('input number 1:  '))
+num2 = int(input('input number 2:  '))
+sum = num1 + num2
+diff = num1 - num2
+prod = num1 * num2
+print("sum:", sum)
+print("difference of numbers:", diff)
+print("product of numbers:", prod)
