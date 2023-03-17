43b245b56ce43fd5fb50dcb8be1edb8f59b43aa0
Laptop <Laptop@DESKTOP-BVI07RL>
2023-03-17 23:21:54 +0200

    task5_done

--- variables.py
+++ variables.py
@@ -1,1 +1,7 @@
 
+width = float(input("input the width of the rectangle: "))  
+height = float(input("input the height of the rectangle: ")) 
+area = width * height  
+print("The area of the rectangle is:", area) 
+
+
