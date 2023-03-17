74155db3f7e09b6c52f1fefcecbb08312e15269f
Laptop <Laptop@DESKTOP-BVI07RL>
2023-03-17 23:21:29 +0200

    task4_done

--- variables.py
+++ variables.py
@@ -1,1 +1,5 @@
 
+number = float(input("input your number: "))
+percentage = float(input("input the percentage you want to calculate: "))
+percent = number / 100 * percentage 
+print("percentage of your number is:", percent)
